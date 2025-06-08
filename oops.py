bfc4138822be3ce26a83030b2d899d0a056a305c
vishnushankar1 <vishnushankar123123@gmail.com>
2025-06-08 11:40:29 +0530

    classes basics done

---
+++ oops.py
@@ -0,0 +1,18 @@
+class employee:
+
+    def __init__(self):
+        print("Started Executing Attributes")
+        self.id=123
+        self.salary=50000
+        self.name="John"
+        print("Attributes has been initiated") 
+
+    def display(self,des):
+        print("This Function was called manulllay")
+        print(f"Employee ID: {self.id}")
+        print(f"Salary Of the Employee : {self.salary}")
+        print(f"Name Of the Employee : {self.name}")
+        print(f"{self.name} is Travelling to {des}")
+
+obj=employee()
+print(obj.display("kochi"))
